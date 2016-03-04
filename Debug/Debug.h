169f743083c1fc133c1feb9b03628a934097e346
christian otkjær <christian.otkjaer@gmail.com>
2016-03-04 09:54:28 +0100

    Initial Commit

---
+++ Debug/Debug.h
@@ -0,0 +1,19 @@
+//
+//  Debug.h
+//  Debug
+//
+//  Created by Christian Otkjær on 04/03/16.
+//  Copyright © 2016 Christian Otkjær. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Debug.
+FOUNDATION_EXPORT double DebugVersionNumber;
+
+//! Project version string for Debug.
+FOUNDATION_EXPORT const unsigned char DebugVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Debug/PublicHeader.h>
+
+
